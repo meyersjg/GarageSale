bfb54e4a630ae16ea1f1cca5a6a1b4396e61a7e1
John Meyers <meyersjg@miamioh.edu>
2013-10-23 17:30:08 -0500

    Initial Commit for RCAV Challenge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GarageSale::Application.config.secret_key_base = '9a0a4c3332a2252b41d105b14c28c5fe4d03b12c965c618968c56b096ec77f92fb5586adbbb73a457385a109d4373bb19c3a11351a805778509a7cc639385aa9'
